9e5c7093173d626855f1f6d372e223871f875197
samar <samar.elkasaas@gmail.com>
2020-12-04 02:18:20 +0200

    Populate Genres Table

---
+++ Data/Migrations/20201203204359_overrirdeConventionsforgenres.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class overrirdeConventionsforgenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
---
+++ Data/Migrations/20201204001009_PopulatingGenresTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class PopulatingGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Genres(Name) values ('Jazz')");
+            migrationBuilder.Sql("insert into Genres(Name) values ('Blues')");
+            migrationBuilder.Sql("insert into Genres(Name) values ('Rock')");
+            migrationBuilder.Sql("insert into Genres(Name) values ('Country')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from Genres where Id in (1,2,3,4)");
+        }
+    }
+}
